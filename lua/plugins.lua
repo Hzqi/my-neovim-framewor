610052d4b7a492e97f0bb5803db66a5bdef00a21
Hzqi <Hzqi_110@hotmail.com>
2023-04-11 23:48:24 +0800

    修改插件自动保存

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -37,3 +37,15 @@ packer.startup({
         },
     },
 })
+
+
+-- 每次保存 plugins.lua 自动安装插件
+pcall(
+  vim.cmd,
+  [[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)
